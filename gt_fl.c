405872f4702786c4ce76ff99464ec70fa1617fab
Abdullah <abdallahmosbah25@gmail.com>
2023-07-24 09:07:38 -0700

    update -fix gt_fl

--- gt_fl.c
+++ gt_fl.c
@@ -3,8 +3,8 @@
 /**
  * get_flag - turns on flags if _printf finds
  * a flag modifier in the format string
- * @s: character that holds the flag specifier
- * @f: pointer to the struct flags in which we turn the flags on
+ * @x: character that holds the flag specifier
+ * @pf: pointer to the struct flags in which we turn the flags on
  * Return: 1 if a flag has been turned on, 0 otherwise
  */
 int get_flag(char x, fl_t *pf)
