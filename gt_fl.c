48662a814980469563c39c36394eeab3ad8ff479
Abdullah <abdallahmosbah25@gmail.com>
2023-07-24 08:57:07 -0700

    get flag adding

---
+++ gt_fl.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * get_flag - turns on flags if _printf finds
+ * a flag modifier in the format string
+ * @s: character that holds the flag specifier
+ * @f: pointer to the struct flags in which we turn the flags on
+ * Return: 1 if a flag has been turned on, 0 otherwise
+ */
+int get_flag(char x, fl_t *pf)
+{
+	int i = 0;
+
+	switch (x)
+	{
+		case '+':
+			pf->plus = 1;
+			i = 1;
+			break;
+		case ' ':
+			pf->space = 1;
+			i = 1;
+			break;
+		case '#':
+			pf->hash = 1;
+			i = 1;
+			break;
+	}
+
+	return (i);
+}
